b5b3f0466ff668dc209fa673b3e5b0fa10af8375
wartman4404 <wartman4404@my.mstc.edu>
2014-11-25 23:55:06 -0600

    inubrush!! idle anim is wrong

---
+++ src/main/unibrushes/inubrush/neko.lua
@@ -0,0 +1,8 @@
+function main(a, b, x, y, points)
+  local statex = math.random(4) - 1
+  local statey = math.random(8) - 1
+  local out = ShaderPaintPoint(a.x, a.y, a.time, a.size, a.speedx, a.speedy, a.distance, a.counter)
+  out.speedx = statex / 4
+  out.speedy = statey / 9
+  pushpoint(points, 0, out)
+end
